6ad12aae417136879a544a7bb95d4037f97b8869
Akvian <31991993+AnnaV1@users.noreply.github.com>
2020-09-10 08:48:28 +0300

    Create theme.js

---
+++ assets/js/theme.js
@@ -0,0 +1,11 @@
+//Get the button:
+window.onload = function() {var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}}
