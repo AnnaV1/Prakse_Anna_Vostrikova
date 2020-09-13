3ebd05b6d879a004e7ca1a0a7870e1379a95222d
Akvian <31991993+AnnaV1@users.noreply.github.com>
2020-09-13 12:36:58 +0300

    Add files via upload

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,36 +1,7 @@
-//Get the button:
-window.onload = function() {var mybutton = document.getElementById("myBtn");
+function openNav() {
+  document.getElementById("myNav").style.height = "100%";
+}
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}}
-
-$(document).ready(function(){
-  // Add smooth scrolling to all links
-  $(document.getElementById("myBtn")).on('click', function(event) {
-
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
-
-      // Store hash
-      var hash = this.hash;
-
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 10000, function(){
-
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
-  });
-});
+function closeNav() {
+  document.getElementById("myNav").style.height = "0%";
+}
