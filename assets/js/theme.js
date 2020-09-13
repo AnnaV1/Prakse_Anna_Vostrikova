09c23a31baf36708c274b285e16c5d2bc3e59203
Akvian <31991993+AnnaV1@users.noreply.github.com>
2020-09-13 23:41:10 +0300

    Add files via upload

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,7 +1,19 @@
 function openNav() {
-  document.getElementById("myNav").style.height = "100%";
+  document.getElementById("mynav").style.height = "100%";
 }
 
 function closeNav() {
-  document.getElementById("myNav").style.height = "0%";
+  document.getElementById("mynav").style.height = "0%";
+}
+
+//Get the button:
+window.onload = function() {var mybutton = document.getElementById("myBtn");
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
 }
